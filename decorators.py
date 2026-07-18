6c14a599b4a4da1b56a6254f15253f84e89e7333
Sathyakrish097 <sathyakrishaji43@gmail.com>
2026-07-18 19:23:29 +0530

    Code Updated

--- decorators.py
+++ decorators.py
@@ -1,5 +1,15 @@
 import time
 
+def time_taken(base_fn):
+    def enhance_fn():
+        start_time= time.time()
+        base_fn()
+        end_time= time.time()
+        total_time= end_time-start_time
+        print(f"Time taken: {total_time} seconds")
+    return enhance_fn 
+
+@time_taken
 def tea():
     print("Preparing Tea....")
     time.sleep(1)
