5e5e2ed35e76dce08d05b5ef2e4b245265952169
Sathyakrish097 <sathyakrishaji43@gmail.com>
2026-07-18 18:46:18 +0530

    decorator python file added

---
+++ decorators.py
@@ -0,0 +1,13 @@
+import time
+
+def tea():
+    print("Preparing Tea....")
+    time.sleep(1)
+    print("Tea is ready to drink!..")
+    
+def main():
+    tea()
+    
+if __name__ == "__main__":
+    main()
+    
