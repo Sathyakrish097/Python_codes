ffdbcc36dc72a8b5bc2d01e93171f6f8fed0238c
Sathyakrish097 <sathyakrishaji43@gmail.com>
2026-07-18 19:31:08 +0530

    Code updated

--- decorators.py
+++ decorators.py
@@ -7,6 +7,7 @@ def time_taken(base_fn):
         end_time= time.time()
         total_time= end_time-start_time
         print(f"Time taken: {total_time} seconds")
+        print("==============================================")
     return enhance_fn 
 
 @time_taken
@@ -14,9 +15,16 @@ def tea():
     print("Preparing Tea....")
     time.sleep(1)
     print("Tea is ready to drink!..")
-    
+ 
+@time_taken  
+def coffee():
+    print("Preparing Coffee....")
+    time.sleep(1)
+    print("Coffee is ready to drink!..")
+
 def main():
     tea()
+    coffee()
     
 if __name__ == "__main__":
     main()
